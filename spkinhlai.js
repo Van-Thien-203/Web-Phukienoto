b7499d4d693e2f1f27c6f9e0610d6be660ad5a8b
VanThien <phamthien182003@gmail.com>
2024-11-17 20:44:30 +0700

    Index.html

---
+++ spkinhlai.js
@@ -0,0 +1,33 @@
+[
+    {
+        "name": "Kính Trước 01",
+        "image": "kinhtruoc1.png",
+        "price": "1.600.000"
+    },
+    {
+        "name": "Kính Trước 02",
+        "image": "kinhtruoc2.png",
+        "price": "1.000.000"
+    },
+    {
+        "name": "Kính Trước 03",
+        "image": "kinhtruoc3.png",
+        "price": "2.100.000"
+    },
+
+    {
+        "name": "Kính Trước 04",
+        "image": "kinhtruoc4.png",
+        "price": "2.200.000"
+    },
+    {
+        "name": "Kính Trước 05",
+        "image": "kinhtruoc5.png",
+        "price": "1.300.000"
+    },
+    {
+        "name": "Kính Trước 06",
+        "image": "kinhtruoc6.png",
+        "price": "1.500.000"
+    }
+]
---
+++ sptrangchu.js
@@ -0,0 +1,63 @@
+[
+    {
+        "name": "Kính sau 03",
+        "image": "kinhsau3.png",
+        "price": "1.600.000"
+    },
+    {
+        "name": "Kính trước 05",
+        "image": "kinhtruoc5.png",
+        "price": "1.000.000"
+    },
+    {
+        "name": "Kính cửa trước 03",
+        "image": "kinhcuatruoc3.png",
+        "price": "2.100.000"
+    },
+    {
+        "name": "Kính sau 06",
+        "image": "kinhsau6.png",
+        "price": "2.200.000"
+    },
+    {
+        "name": "Kính sau 02",
+        "image": "kinhsau2.png",
+        "price": "1.300.000"
+    },
+
+    {
+        "name": "Kính sau 01",
+        "image": "kinhsau1.png",
+        "price": "3.200.000"
+    },
+    {
+        "name": "Kính sau 04",
+        "image": "kinhsau4.png",
+        "price": "1.700.000"
+    },
+    {
+        "name": "Kính sau 03",
+        "image": "kinhsau3.png",
+        "price": "2.200.000"
+    },
+    {
+        "name": "Kính sau 05",
+        "image": "kinhsau5.png",
+        "price": "1.300.000"
+    },
+    {
+        "name": "Kính sau 06",
+        "image": "kinhsau6.png",
+        "price": "2.200.000"
+    },
+    {
+        "name": "Kính sau 02",
+        "image": "kinhsau2.png",
+        "price": "1.300.000"
+    },
+    {
+        "name": "Kính Trước 02",
+        "image": "kinhtruoc2.png",
+        "price": "1.500.000"
+    }
+]
